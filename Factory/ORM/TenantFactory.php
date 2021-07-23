fc13797697e606146e22c216329aefb68c2432bc
Vlad <vlad@ecomitize.com>
2021-07-23 12:17:49 +0300

    added more options to configuration

--- Factory/ORM/TenantFactory.php
+++ Factory/ORM/TenantFactory.php
@@ -49,12 +49,11 @@ class TenantFactory implements FactoryInterface
         if (empty($this->normalizedData)) {
             throw new \LogicException('Tenant normalized data must be set first, before creating new tenant');
         }
+        $configuration = array_merge($this->defaultConfiguration, array_filter($this->normalizedData['configuration']));
 
         $tenant = new Tenant();
         $tenant->setName($this->normalizedData['name']);
-
-        // TODO implement config change on a fly
-        $tenant->setSettings($this->defaultConfiguration);
+        $tenant->setSettings($configuration);
 
         $this->provider->save($tenant);
 
@@ -87,11 +86,19 @@ class TenantFactory implements FactoryInterface
      */
     protected function optionsConfigure(OptionsResolver $resolver): OptionsResolver
     {
-        $resolver->setRequired(['name', 'email', 'firstname']);
-
+        $resolver->setRequired(['name', 'configuration']);
         $resolver->setAllowedTypes('name', 'string');
-        $resolver->setAllowedTypes('email', 'string');
-        $resolver->setAllowedTypes('firstname', 'string');
+        $resolver->setAllowedTypes('configuration', 'array');
+
+        $resolver->setDefault('configuration', function(OptionsResolver $nestedResolver) {
+            $nestedResolver->setRequired(['host', 'port', 'dbname', 'user', 'password']);
+            $nestedResolver->setDefaults($this->defaultConfiguration);
+            $nestedResolver->setAllowedTypes('host', ['string', 'null']);
+            $nestedResolver->setAllowedTypes('port', ['int', 'null']);
+            $nestedResolver->setAllowedTypes('dbname', ['string', 'null']);
+            $nestedResolver->setAllowedTypes('user', ['string', 'null']);
+            $nestedResolver->setAllowedTypes('password', ['string', 'null']);
+        });
 
         return $resolver;
     }
--- Tests/Unit/Factory/ORM/TenantFactoryTest.php
+++ Tests/Unit/Factory/ORM/TenantFactoryTest.php
@@ -2,9 +2,12 @@
 
 namespace App\TenantBundle\Tests\Unit\Factory\ORM;
 
+use App\TenantBundle\Entity\Tenant;
 use App\TenantBundle\Factory\ORM\TenantFactory;
 use App\TenantBundle\Interfaces\TenantProviderInterface;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
+use Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
 
 /**
  * Class TenantFactoryTest
@@ -27,5 +30,84 @@ class TenantFactoryTest extends TestCase {
         self::expectExceptionMessage('Tenant normalized data must be set first, before creating new tenant');
 
         $this->factory->createNew();
+    }
+
+    public function test_fails_when_wrong_data_passed()
+    {
+        self::expectException(MissingOptionsException::class);
+        self::expectExceptionMessage('The required option "name" is missing.');
+
+        $this->factory->withData(['test']);
+    }
+
+    public function test_fails_when_redundant_data_passed()
+    {
+        $result = $this->factory->withData(['name' => 'foo', 'configuration' => [], 'email' => 'test'])->createNew();
+        self::assertEquals(true, true);
+        self::assertEquals('foo', $result->getName());
+        self::assertEquals([
+            'host' => '127.0.0.1',
+            'port' => 3306,
+            'dbname' => 'guest',
+            'user' => 'test',
+            'password' => 'pass'
+        ], $result->getSettings());
+    }
+
+    public function test_fails_when_wrong_type_passed()
+    {
+        self::expectException(InvalidOptionsException::class);
+        self::expectExceptionMessage('The option "name" with value 1 is expected to be of type "string", but is of type "int".');
+
+        $this->factory->withData(['name' => 1, 'configuration' => []]);
+    }
+
+    public function test_fails_when_wrong_second_type_passed()
+    {
+        self::expectException(InvalidOptionsException::class);
+        self::expectExceptionMessage('The nested option "configuration" with value "string" is expected to be of type array, but is of type "string".');
+
+        $this->factory->withData(['name' => 'foo', 'configuration' => 'string']);
+    }
+
+    public function test_fails_when_wrong_subtype_type_passed()
+    {
+        self::expectException(InvalidOptionsException::class);
+        self::expectExceptionMessage('The option "configuration[host]" with value 111 is expected to be of type "string" or "null", but is of type "int".');
+
+        $this->factory->withData(['name' => 'foo', 'configuration' => [
+            'host' => 111
+        ]]);
+    }
+
+    public function test_should_apply_default_config_values()
+    {
+        $result = $this->factory->withData(['name' => 'foo', 'configuration' => [
+            'host' => null,
+            'dbname' => 'foo'
+        ]])->createNew();
+        self::assertInstanceOf(Tenant::class, $result);
+        self::assertEquals('foo', $result->getName());
+        self::assertEquals([
+            'host' => '127.0.0.1',
+            'port' => 3306,
+            'dbname' => 'foo',
+            'user' => 'test',
+            'password' => 'pass',
+        ], $result->getSettings());
+    }
+
+    public function test_ok()
+    {
+        $result = $this->factory->withData(['name' => 'foo', 'configuration' => []])->createNew();
+        self::assertInstanceOf(Tenant::class, $result);
+        self::assertEquals('foo', $result->getName());
+        self::assertEquals([
+            'host' => '127.0.0.1',
+            'port' => 3306,
+            'dbname' => 'guest',
+            'user' => 'test',
+            'password' => 'pass',
+        ], $result->getSettings());
     }
 }
